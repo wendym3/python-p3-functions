5d0dc932a64725e2809a0912c30a28e40a81d3dc
wendymasese <wendy.masese@student.moringaschool.com>
2024-06-29 16:48:26 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
+def greet_programmer(name ="programmer"):
+    print(f"Hello, {name}!")
     pass
 
-def greet(name):
+def greet(name = "Guido"):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
-def add(num1, num2):
+def add(num1 = 45, num2 = 55):
+    return(num1 + num2)
+    print(add)
     pass
 
-def halve(number):
+def halve(number = 100):
+    return (number/2)
+    print(halve)
     pass
